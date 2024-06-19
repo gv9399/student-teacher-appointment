5df53760072659e0a26c08a40994deebc93fc843
Gaurav Verma <105119476+gv9399@users.noreply.github.com>
2024-06-19 17:37:24 +0530

    Delete backend directory

--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('./routes');
-
-module.exports.registerApplicationRoutes = function (app) {
-    for (let uri in routes) {
-        app.use(uri, routes[`${uri}`]);
-    }
-};
--- backend/routes/routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    '/api/user': require('./user'),
-    '/api/availability': require('./availability'),
-    '/api/appointment': require('./appointment'),
-};
